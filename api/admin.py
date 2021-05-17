4f67b5fb30530b04d1a6067a09390c52865013ff
ildarick93 <saliakhovif@yandex.ru>
2021-05-17 11:23:55 +0300

    1st commit of 10th sprint

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comment, Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'pub_date', 'author')
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("pk", "author", "text", "created")
+    search_fields = ("author",)
+    list_filter = ("author",)
+    empty_value_display = "-пусто-"
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
