f52ee3b94ab5ad3608f1ce5973094a54f9bf24d9
ildarick93 <saliakhovif@yandex.ru>
2021-05-17 11:19:27 +0300

    1st commit of 10th sprint

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,18 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.routers import DefaultRouter
+
+from .views import CommentViewSet, PostViewSet
+
+router = DefaultRouter()
+router.register('posts', PostViewSet)
+router.register(
+    r'posts/(?P<id>[0-9]+)/comments',
+    CommentViewSet,
+    basename='Comments'
+)
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token),
 ]
