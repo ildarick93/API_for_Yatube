1d76267f67a924df34128840e778940c917cce9d
ildarick93 <saliakhovif@yandex.ru>
2021-05-17 11:15:05 +0300

    1st commit of 10th sprint

--- api/models.py
+++ api/models.py
@@ -4,6 +4,14 @@ from django.db import models
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.CharField(max_length=200, null=False)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(
@@ -12,6 +20,12 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="posts"
     )
+    group = models.ForeignKey(Group, on_delete=models.SET_NULL,
+                              related_name="posts",
+                              blank=True,
+                              null=True,
+                              verbose_name="Группа",
+                              help_text="Выберите группу")
 
     def __str__(self):
         return self.text
@@ -28,3 +42,17 @@ class Comment(models.Model):
     created = models.DateTimeField(
         "Дата добавления", auto_now_add=True, db_index=True
     )
+
+
+class Follow(models.Model):
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "following"], name="unique_follow")
+        ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             verbose_name="Подписчик", related_name="follower")
+    following = models.ForeignKey(User, on_delete=models.CASCADE,
+                                  verbose_name="Подписант",
+                                  related_name="following")
