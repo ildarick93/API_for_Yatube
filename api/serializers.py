2c0c3ac97d91b76506c589f67e03ec270d53f351
ildarick93 <saliakhovif@yandex.ru>
2021-05-18 11:21:04 +0300

    1st commit of 10th sprint

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
 
-from .models import Comment, Follow, Group, Post
+from .models import Comment, Follow, Group, Post, User
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -22,12 +23,38 @@ class CommentSerializer(serializers.ModelSerializer):
 class GroupSerializer(serializers.ModelSerializer):
 
     class Meta:
-        fields = '__all__'
         model = Group
+        fields = '__all__'
+        extra_kwargs = {
+            'description': {'required': False},
+            'slug': {'required': False},
+        }
 
 
 class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
 
     class Meta:
-        fields = '__all__'
         model = Follow
+        fields = ['user', 'following']
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=['user', 'following']
+            )
+        ]
+
+    def validate_following(self, following):
+        if self.context.get('request').method == 'POST':
+            if self.context.get('request').user == following:
+                raise serializers.ValidationError(
+                    'You can not follow to yourself.')
+        return following
